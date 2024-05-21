b641430ebf6004f7807a60693109806bc88784e5
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2024-05-21 21:22:25 +0200

    Update DegreeWelcomeMessage.js

--- app/src/components/DegreeWelcomeMessage.js
+++ app/src/components/DegreeWelcomeMessage.js
@@ -1,10 +1,9 @@
+// DegreeWelcomeMessage.js
 import React from 'react';
 
-const DegreeWelcomeMessage = ({ username, degreeProgram, courseOfStudy }) => {
+const DegreeWelcomeMessage = ({ degreeProgram, courseOfStudy }) => {
   return (
     <div>
-      <h2>ECRI Student Hub</h2>
-      <p>Hello, {username}</p>
       <h2>Welcome to the {degreeProgram} Program: {courseOfStudy}</h2>
     </div>
   );
