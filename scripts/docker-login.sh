02e465c336075d6b9d9ea236277b04cb829f4e0f
Princepaul Izuogu <123191250+PrincepaulIzuogu@users.noreply.github.com>
2024-07-09 11:06:45 +0200

    Update docker-login.sh

--- scripts/docker-login.sh
+++ scripts/docker-login.sh
@@ -6,28 +6,13 @@ if [[ -z "$ACR_USERNAME" || -z "$ACR_PASSWORD" ]]; then
   exit 1
 fi
 
-# Log in to Azure using service principal
-echo "Logging in to Azure..."
-az login --service-principal -u $ACR_USERNAME -p $ACR_PASSWORD --tenant 8775661c-d343-4930-a990-8a3360e2ca1f
-if [[ $? -ne 0 ]]; then
-  echo "Azure login failed"
-  exit 1
-fi
-
-# Get the access token
-echo "Retrieving access token..."
-TOKEN=$(az acr login --name ecristudenthubacr --expose-token | jq -r '.accessToken')
-if [[ -z "$TOKEN" ]]; then
-  echo "Failed to retrieve access token"
-  exit 1
-fi
-
-# Login to Docker using the token
+# Login to Docker using ACR credentials
 echo "Logging in to Docker..."
-echo $TOKEN | docker login ecristudenthubacr.azurecr.io -u 00000000-0000-0000-0000-000000000000 --password-stdin
+echo $ACR_PASSWORD | docker login ecristudenthubacr.azurecr.io -u $ACR_USERNAME --password-stdin
 if [[ $? -ne 0 ]]; then
   echo "Docker login failed"
   exit 1
 fi
 
 echo "Docker login successful"
+
